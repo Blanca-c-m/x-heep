262fbaab92f8321008d7959126a39a5f111237ec
Blanca-c-m <blanca.calvo@alumnos.upm.es>
2026-02-03 16:53:00 +0100

    Update max.c

--- sw/applications/tfg_blanca/max.c
+++ sw/applications/tfg_blanca/max.c
@@ -1,14 +1,18 @@
 #include "max.h"
 
 void max(int input[], int length, int *valor, int *indice){
-    *valor = input[0];
-    *indice= 0;
+
+   int max_val = input[0];
+   int max_idx = 0;
 
     for(int i=1; i < length;  i++){
-        if(input[i] > *valor){
-            *valor= input[i];
-            *indice = i;
+        if(input[i] > max_val){
+            max_val= input[i];
+            max_idx = i;
         }
 
     }
+    
+    *valor = max_val;
+    *indice = max_idx;
 }
