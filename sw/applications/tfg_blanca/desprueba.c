a3da25264a85ec09811815915730fb6455b2177f
Blanca-c-m <blanca.calvo@alumnos.upm.es>
2026-02-03 16:49:03 +0100

    Update desprueba.c

--- sw/applications/tfg_blanca/desprueba.c
+++ sw/applications/tfg_blanca/desprueba.c
@@ -1,132 +1,132 @@
-#include "desprueba.h"
-#define escala 1000
+#include "desprueba.h" 
 
-
- 
-void build_diagonales(long long int diag0[], long long int diag1[], long long int diag_1[], long long int diag2[], long long int diag_2[], int lambda) {
-
-    long long int lambda_sq = (long long int)lambda * lambda;
-
+// DESCOMPOSICION DE LA SEÑAL CON LA MEJORA DE QUE ALMACENA SOLO LAS DIAGONALES, EN ESTA FUNCION SE SIGUE DIVIDIENDO.
+void print_diagonals_v1(int diag0[], int diag1[], int diag_1[], int diag2[], int diag_2[]) {
     int N = signal_length;
-    // Diagonal Principal (d0)
+
+    printf("\nDiagonal Principal:\n");
     for (int i = 0; i < N; i++) {
-        long long int D2tD2 = 0;
-        // Coeficientes de la diagonal de D2^T * D2 (1, 5, 6) escalados.
-        if (i == 0 || i == N - 1) D2tD2 = 1 * escala;
-        else if (i == 1 || i == N - 2) D2tD2 = 5 * escala;
-        else D2tD2 = 6 * escala;
-
-        // diag0[i] = I[i,i] + lambda^2 * D2tD2[i,i]. I[i,i] es 'escala'.
-        diag0[i] = escala + (lambda_sq * D2tD2);
+        printf("%d ", diag0[i]);
     }
-    // Diagonal +1 y -1 (d1 y d_1): A[i, i+1] ---
+
+    printf("\nDiagonal Superior:\n");
     for (int i = 0; i < N - 1; i++) {
-
-        long long int D2tD2 = 0;
-        // Coeficientes de la diagonal +1 de D2^T * D2 (-2, -4) escalados.
-
-        if (i == 0 || i == N - 2) D2tD2 = -2 * escala;
-        else D2tD2 = -4 * escala;
-        diag1[i] = (lambda_sq * D2tD2);
-        diag_1[i + 1] = diag1[i]; // Simetría A[i+1, i] = A[i, i+1]
-
+        printf("%d ", diag1[i]);
     }
+
+    printf("\nDiagonal Inferior:\n");
+    for (int i = 1; i < N; i++) {
+        printf("%d ", diag_1[i]);
+    }
+    
+    printf("\nDiagonal Superior Segunda:\n");
     for (int i = 0; i < N - 2; i++) {
+        printf("%d ", diag2[i]);
+    }
 
-        long long int D2tD2_ip2 = 1 * escala;
-        diag2[i] = (lambda_sq * D2tD2_ip2);
-        diag_2[i+2] = diag2[i];
-
+    printf("\nDiagonal Inferior Segunda:\n");
+    for (int i = 2; i < N; i++) {
+        printf("%d ", diag_2[i]);
     }
 
 }
 
+void build_diagonales_v1(int diag0[], int diag1[], int diag_1[], int diag2[], int diag_2[], int lambda) {
 
-// ELIMINACION POR TRIANGULAR SUPERIOR
-void forward_elimination(long long int diag0[], long long int diag1[], long long int diag2[], long long int b[], long long int diag_1[], long long int diag_2[]) {
-    long long int factor;
+    int lambda_sq = (int)lambda * lambda;
+
     int N = signal_length;
+
+    for (int i = 0; i < N; i++) {
+        int D2tD2 = 0;
+
+        if (i == 0 || i == N - 1) D2tD2 = 1 * scale;
+        else if (i == 1 || i == N - 2) D2tD2 = 5 * scale;
+        else D2tD2 = 6 * scale;
+
+        diag0[i] = scale + (lambda_sq * D2tD2);
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+
+        int D2tD2 = 0;
+
+        if (i == 0 || i == N - 2) D2tD2 = -2 * scale;
+        else D2tD2 = -4 * scale;
+
+        diag1[i] = (lambda_sq * D2tD2);
+        diag_1[i + 1] = diag1[i];
+    }
+
     for (int i = 0; i < N - 2; i++) {
 
-        long long int pivote = diag0[i];
+        int D2tD2_ip2 = 1 * scale;
+        diag2[i] = (lambda_sq * D2tD2_ip2);
+        diag_2[i + 2] = diag2[i];
+    }
+}
 
-        factor = diag_1[i+1]; 
-        // ACTUALIZO i + 1
+void forward_elimination_v1(int diag0[], int diag1[], int diag2[], int b[], int diag_1[], int diag_2[]) {
+    int factor;
+    int N = signal_length;
+
+    for (int i = 0; i < N - 2; i++) {
+
+        int pivote = diag0[i];
+
+        factor = diag_1[i + 1];
         diag0[i + 1] -= (factor * diag1[i]) / pivote;
         diag1[i + 1] -= (factor * diag2[i]) / pivote;
         b[i + 1] -= (factor * b[i]) / pivote;
 
-        factor = diag_2[i+2];
-        //ACTUALIZO i + 2
+        factor = diag_2[i + 2];
         diag_1[i + 2] -= (factor * diag1[i]) / pivote;
         diag0[i + 2] -= (factor * diag2[i]) / pivote;
         b[i + 2] -= (factor * b[i]) / pivote;
-        
     }
-
-   
-
-    //Última eliminación
 
     {
         int i = N - 2;
 
-        long long int pivote = diag0[i];
+        int pivote = diag0[i];
 
-        factor = diag_1[i+1]; // A[N-1, N-2]
-        // Actualizar diag0[N-1]
-
+        factor = diag_1[i + 1];
         diag0[i + 1] -= (factor * diag1[i]) / pivote;
-
-        // Actualizar b[N-1]
         b[i + 1] -= (factor * b[i]) / pivote;
-
     }
-
 }
 
-// SUSTITUCIÓN HACIA ATRÁS
-
-void back_substitution(long long int diag0[], long long int diag1[], long long int diag2[], long long int b[], long long int x[], long long int ph[]) {
+void back_substitution_v1(int diag0[], int diag1[], int diag2[], int b[], int x[], int ph[]) {
     int N = signal_length;
 
-    // x[N-1] = b[N-1] / A'[N-1, N-1]
-    x[N - 1] = (b[N - 1]*escala) / diag0[N - 1];
+    x[N - 1] = (b[N - 1] * scale) / diag0[N - 1];
 
-    long long int term = (diag1[N - 2] * x[N - 1]) / escala; 
-    x[N - 2] = ((b[N - 2] - term)*escala) / diag0[N - 2];
+    int term = (diag1[N - 2] * x[N - 1]) / scale;
+    x[N - 2] = ((b[N - 2] - term) * scale) / diag0[N - 2];
 
-    // Bucle para el resto (desde N-3 hasta 0)
     for (int i = N - 3; i >= 0; i--) {
-        long long int sum = 0;
+        int sum = 0;
 
-        sum += (diag1[i] * x[i + 1]) / escala;
+        sum += (diag1[i] * x[i + 1]) / scale;
+        sum += (diag2[i] * x[i + 2]) / scale;
 
-        sum += (diag2[i] * x[i + 2]) / escala;
-
-        x[i] = ((b[i] - sum))*escala/ diag0[i];
-
+        x[i] = ((b[i] - sum) * scale) / diag0[i];
     }
 }
 
-
-void solve(long long int diag0[], long long int diag1[], long long int diag2[], long long int diag_1[], long long int diag_2[],long long int b[], long long int x[], long long int ph[]) {
+void solve_v1(int diag0[], int diag1[], int diag2[], int diag_1[], int diag_2[], int b[], int x[], int ph[]) {
     int N = signal_length;
-    long long int b_original[N];
+    int b_original[N];
 
     for (int i = 0; i < N; i++) {
         b_original[i] = b[i];
     }
-    //Ejecutar la fase de eliminación
-    forward_elimination(diag0, diag1, diag2, b, diag_1,diag_2);
-    //Ejecutar la sustitución regresiva
-    back_substitution(diag0, diag1, diag2, b, x, ph);
 
+    forward_elimination_v1(diag0, diag1, diag2, b, diag_1, diag_2);
+    back_substitution_v1(diag0, diag1, diag2, b, x, ph);
 
     for (int i = 0; i < N; i++) {
-        // Asumiendo que b_original y x están ambos escalados por 'escala'
-        ph[i] = b_original[i] - x[i]; 
+        ph[i] = b_original[i] - x[i];
     }
-
 }
 
