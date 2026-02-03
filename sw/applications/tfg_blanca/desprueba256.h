2113f87ad46f2e7562b3fdd44921675c36cb23eb
Blanca-c-m <blanca.calvo@alumnos.upm.es>
2026-02-03 16:50:22 +0100

    Create desprueba256.h

---
+++ sw/applications/tfg_blanca/desprueba256.h
@@ -0,0 +1,6 @@
+#include "signal_EDA_shift.h"
+
+void build_diagonales_v2(int diag0[], int diag1[], int diag_1[], int diag2[], int diag_2[], int lambda);
+void print_diagonals_v2(int diag0[], int diag1[], int diag_1[], int diag2[], int diag_2[]);
+void solve_v2(int diag0[], int diag1[], int diag2[], int diag_1[], int diag_2[], int b[], int x[], int ph[]);
+int shift_div(int num, int den);
